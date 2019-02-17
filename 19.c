0473776a836b70d07b276437f4cfb036804ee119
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-02-17 22:15:54 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,22 @@
+int main() {
+  int a,i,b[100],k=0,j,l=0,m;
+  scanf("%d",&a);
+  for(i=2;i<=a;i++)
+  {
+      if(a%i==0)
+  {
+  for(m=2;m<i;m++)
+  {
+      if(i%m==0)
+      {
+      l++;
+      break;
+      }
+   }
+  
+  if(l==0)
+  {
+  printf("%d\t",i);
+  }}}
+  return 0;
+}
