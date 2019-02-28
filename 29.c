822aac61dff642467cc29dace55eac02f2f6a91b
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-02-28 06:27:17 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,15 @@
+int main() {
+  int a,b,i,l=0,j;
+  scanf("%d%d",&a,&b);
+  for(i=a;i<b;i++)
+  {
+      for(j=2;j<i;j++)
+      {
+          if(j*j==i)
+          {
+             l++;
+          }
+      }
+  }
+      printf("%d",l);
+}
