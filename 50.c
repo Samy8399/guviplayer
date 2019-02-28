2a2f452c3c64b10d3a5686cf815390560106be40
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-02-28 06:43:36 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,22 @@
+int main() 
+{
+	int n,i,a=0;
+	scanf("%d",&n);
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			=1;
+			break;
+		}
+	}
+	if(a==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+
+}
