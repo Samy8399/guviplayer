6d199e4d97e149030edb40ebbaf20d8e1a75619e
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-02-28 06:50:22 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,20 @@
+int main() {
+   char a[100];
+   int i,j,l;
+   scanf("%d",&l);
+   scanf("%s",a);
+   for(i=0;i<l;i++)
+   {
+       for(j=i+1;j<l;j++)
+       {
+           if(a[i]=='1')
+           {
+               if(a[j]=='0')
+               {
+                   printf("1");
+                   break;
+               }
+           }
+       }
+   }
+}
