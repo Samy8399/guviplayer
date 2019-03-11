b4f688be1e5dfaf8fde01afab3b1d1180ffe2af0
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-03-11 12:38:07 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],b,i,j,c,d=0;
+	scanf("%d",&b);
+	for(i=0;i<b;i++)
+	{
+	scanf("%d",&a[i]);
+		}
+	//printf("%d\n",a[0]);
+	for(i=0;i<b;i++)
+	{
+		for(j=0;j<b;j++)
+		{
+			c=(a[i]&a[j]);
+			printf("%d",c);
+		}
+	}
+}
