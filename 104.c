e400a74cb5a3804235d4c916e009a0350ed6d8f7
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-03-17 22:59:48 +0530

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int a[100],b,c=0,d=0,i,j;
+	scanf("%d",&b);
+	for(i=0;i<b;i++)
+	scanf("%d",&a[i]);
+	for(j=0;j<b-1;j++)
+	{
+	c=a[j]+a[j+1];
+
+	d=d+c;
+	printf("d=%d ",d);
+	}
+	printf("\n%d",d);
+}
