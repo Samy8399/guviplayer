fe281e298ed292c7b8e9553a30fd8b5a999b8b27
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-03-18 07:44:26 +0530

    Create 138.c

---
+++ 138.c
@@ -0,0 +1,18 @@
+int main()
+{
+	int a,b=2,c=1,d=0;
+	scanf("%d",&a);
+	while(b)
+	{
+		c=c*2;
+		if(a==c)
+		{
+		printf("yes");
+		break;
+		}
+		else if(c>a){
+			printf("no");
+			break;
+	}
+}
+}
