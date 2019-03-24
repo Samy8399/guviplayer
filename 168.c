7e8f753204e70c9c99c67b449bbbf32d9fdf111e
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-03-24 13:56:20 +0530

    Create 168.c

---
+++ 168.c
@@ -0,0 +1,16 @@
+int main()
+{
+	char a[100];
+	int b,c,d,e,i,j,k;
+	scanf("%s",a);
+	b=strlen(a);
+	for(i=0;i<b;i++)
+	{
+		if(a[i]>='1' && a[i]<='9')
+        {
+        	c=a[i]-48;
+        	for(j=0;j<c;j++)
+        	printf("%c",a[i-1]);
+        }
+     }
+}
