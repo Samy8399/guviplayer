6567522bdca5f0034ef3341a7401f35a85068104
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-03-24 22:22:18 +0530

    Create 176.c

---
+++ 176.c
@@ -0,0 +1,24 @@
+int main()
+{
+	char a[100],b[100];
+	int c,d,e=0,f,i,j,k;
+	scanf("%s",a);
+	gets(b);
+	c=strlen(a);
+	d=strlen(b);
+	for(i=0;i<c;i++)
+	{
+		for(j=0;j<d;j++)
+		{
+			if(a[i]==b[j])
+			{
+				b[j]='\0';
+				e++;
+			}
+		}
+	}
+	if(e<=c)
+	printf("yes");
+	else
+	printf("no");
+}
