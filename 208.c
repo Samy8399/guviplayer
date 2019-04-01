90dd641d07ed67a943f296716c7f1af0385d14c3
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-04-01 16:23:36 +0530

    Create 208.c

---
+++ 208.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+   int a[100][100],b,c,d=0,e=0,f,i,j,k;
+   scanf("%d",&b);
+   for(i=0;i<b;i++)
+   {
+    for(j=0;j<b;j++)
+      scanf("%d",&a[i][j]);
+   }
+   for(i=0;i<b;i++)
+   {
+       for(j=0;j<b;j++)
+       {
+           if(i==j)
+           d=d+a[i][j];
+       }
+   }
+    for(i=0;i<b;i++)
+   {
+       for(j=0;j<b;j++)
+       {
+           if((i+j)==(b-1))
+           e=e+a[i][j];
+       }
+   }
+    f=e*d;
+    printf("%d",f);
+}    
